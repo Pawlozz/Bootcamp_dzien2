0e7f4e5773bdb52aba0f727c2b428b4d166a5826
Pawelo <golata.pawel06@gmail.com>
2024-06-25 20:10:21 +0200

    Finisched site and styles

--- src/dzien2_frontend/src/App.vue
+++ src/dzien2_frontend/src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 import { ref } from 'vue';
 import { dzien2_backend } from 'declarations/dzien2_backend/index';
+import Blog from './components/Blog.vue';
+
 let greeting = ref('');
 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
   const name = target.querySelector('#name').value;
-  await dzien2_backend.greet(name).then((response) => {
+  const surname = target.querySelector('#surname').value;
+  await dzien2_backend.greet(name, surname).then((response) => {
     greeting.value = response;
   });
 }
@@ -19,10 +22,17 @@ async function handleSubmit(e) {
     <br />
     <br />
     <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
+      <div>
+        <label for="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+      </div>
+      <div>
+        <label for="surname">Enter your Surname: &nbsp;</label>
+        <input id="surname" alt="Surname" type="text" />
+      </div>
       <button type="submit">Click Me!</button>
     </form>
     <section id="greeting">{{ greeting }}</section>
+    <Blog/>
   </main>
 </template>
---
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+    <div class="mainBlog">
+        <hr>
+        <button @click="pobierzWpisy">Pobierz wpisy</button>
+        <h2>Siema Blog!</h2>
+        {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">Dodaj</button>
+    </div>
+</template>
+<script>
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
+
+export default {
+    data(){
+        return{
+            wpisy:  [],
+            nowyBlog: ""
+        }
+    },
+    methods: {
+        async dodajWpisy(){
+            await dzien2_backend.dodaj_wpis(this.nowyBlog);
+        },
+        async pobierzWpisy(){
+            this.wpisy = await dzien2_backend.odczytaj_wpisy();
+        }
+    }
+}
+</script>
+<style lang="scss">
+    hr{
+        width: calc(25vw + 10%);
+        color: #582785;
+    }
+
+    .mainBlog{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        gap: 2rem;
+    }
+</style>
